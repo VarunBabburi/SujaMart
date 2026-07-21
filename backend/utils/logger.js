44d37ef677d41605693041ba92f3fdbf8cbfb39b
VarunBabburi <varunbabburi@gmail.com>
2026-07-21 17:35:33 +0530

    Fix logger directory creation

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,28 +1,30 @@
 const fs = require("fs");
 const path = require("path");
 
-const logFile = path.join(__dirname, "../logs/app.log");
+const logDir = path.join(__dirname, "../logs");
+const logFile = path.join(logDir, "app.log");
+
+// Create logs directory if it doesn't exist
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir, { recursive: true });
+}
 
 const writeLog = (level, data) => {
   const log = {
-    time: new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }),
+    time: new Date().toLocaleString("en-US", {
+      timeZone: "Asia/Kolkata",
+    }),
     level,
     ...data,
   };
 
-  fs.appendFile(
-    logFile,
-    JSON.stringify(log) + "\n",
-    (err) => {
-      if (err) {
-        console.error("Logger Error:", err);
-      }
+  fs.appendFile(logFile, JSON.stringify(log) + "\n", (err) => {
+    if (err) {
+      console.error("Logger Error:", err);
     }
-  );
+  });
 };
 
 exports.info = (data) => writeLog("INFO", data);
-
 exports.warn = (data) => writeLog("WARN", data);
-
 exports.error = (data) => writeLog("ERROR", data);
